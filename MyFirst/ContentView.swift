c5dca3dc40a9cc9cea290557b40bb05c0fadafa4
gadgelogger <sarayou273142910@gmail.com>
2023-12-07 22:27:40 +0900

    Initial Commit

---
+++ MyFirst/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFirst
+//
+//  Created by gadgelogger on 2023/12/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by gadgelogger on 2023/12/07.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
